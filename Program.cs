1174a685704ef2b11a5d0d816559ceef09ea826b
fatih-git <72505641+fatih-git@users.noreply.github.com>
2022-12-07 16:23:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Deneme2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string isim = "Fatih";
+            int a = 7;
+            yazdir(a);
+            yazdir(isim);
+
+            int x=5;
+            int y = 7;
+            int z;
+            int sonuc;
+
+            sonuc = toplam(x, ref y, out z);
+            Console.WriteLine(sonuc);
+            Console.WriteLine(x+" "+y+" "+z);
+
+            /*
+             ref ve out pointer gibi çalışır.
+             ref kullanırken fonksiyona verilecek değerin bir başlangıç değeri olması gerekmektedir.
+             out böyle bir şey istemez.
+             out ve ref arasındaki fark budur.
+             */
+
+        }
+
+        //Overload
+        public static void yazdir(int deger)
+        {
+            Console.WriteLine(deger);
+        }
+        public static void yazdir(string deger)
+        {
+            Console.WriteLine(deger);
+        }
+
+        //out kullanımı
+        public static int toplam(int k, ref int l, out int m)
+        {
+            m = 7;
+            l++;
+            int sum = k + l + m;
+
+            return sum;
+        }
+
+    }
+}
